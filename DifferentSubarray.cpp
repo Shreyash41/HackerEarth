494efb400a21165527ff93fed8c9f6ff67133e7a
SHREYASH ASATI <62322907+Shreyash41@users.noreply.github.com>
2020-05-23 22:52:48 +0530

    May Circuit'20
    HackerEarth

---
+++ DifferentSubarray.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// void input()
+// { 
+//     #ifndef ONLINE_JUDGE 
+//     freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+//     #endif
+// }
+#define endl "\n"
+#define ll long long int
+#define vll vector<ll>
+#define pb push_back
+#define pll pair<ll,ll>
+#define mp make_pair
+#define ff first
+#define ss second
+#define tll tuple<ll,ll,ll>
+#define mt make_tuple
+#define INF LONG_LONG_MAX
+#define mod 1000000007
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define rrep(i,a,b) for(ll i=a;i>=b;i--)
+#define cases ll t; cin>>t; while(t--)
+#define array(a,n) ll a[n]; rep(i,0,n) cin>>a[i];
+#define vector(a,n) vll a(n); rep(i,0,n) cin>>a[i];
+#define sortall(a) sort(a.begin(),a.end())
+#define printall(a,n) rep(i,0,n) cout<<a[i]<<" "; cout<<endl;
+ 
+// --------------- My Template End --------------- //
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+    //input();
+    ll n,num=2,plus=1;
+    cin>>n;
+    rep(i,0,n)
+    {
+        cout<<num<<" ";
+        num+=plus;
+        plus=pow(2,int(log2(num)-1));
+    }
+}
