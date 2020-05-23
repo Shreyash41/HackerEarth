6fc0659dae43109372e7913d0b56dd9e0cb6c1ed
SHREYASH ASATI <62322907+Shreyash41@users.noreply.github.com>
2020-05-23 22:56:49 +0530

    May Circuit'20
    HackerEarth

---
+++ ShortestPath.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false);
+#define ull unsigned long long int
+#define ll long long int 
+#define F(i,a,b) for(int i=(int)a;i<(int)b;i++)
+ 
+int main(){
+	// #ifndef ONLINE_JUDGE
+    //    freopen("input.txt","r",stdin);
+    //    freopen("output.txt","w",stdout);
+    // #endif 
+	int n,m,k;
+	cin>>n>>m>>k;
+F(i,0,m){
+	int a,b,c;
+	cin>>a>>b>>c;
+}
+F(i,0,n){
+	cout<<0<<" ";
+}
+}
